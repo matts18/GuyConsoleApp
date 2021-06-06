61c7747040566bf5aacb9f4ad07f703d418556d6
matts18 <snelgarm@gmail.com>
2021-06-06 12:10:19 +0800

    create console app

---
+++ MoneyTracking/Guy.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace MoneyTracking
+{
+    public class Guy
+    {
+        public string Name;
+        public int Cash;
+
+        public Guy()
+        {
+        }
+
+        /// <summary>
+        /// Writes my name and the amount of cash I have to the console.
+        /// </summary>
+        public void WriteMyInfo()
+        {
+            Console.WriteLine($"{Name} has {Cash} bucks.");
+        }
+
+        /// <summary>
+        /// Gives some of my cash and removes it from my wallet (or prints
+        ///  a message to the console if I dont have enough cash).
+        /// </summary>
+        /// <param name="amount">Amount of cash to give.</param>
+        /// <returns>The amount of cash removed from my wallet or 0 if I
+        ///  dont have enough or the amount is invalid.</returns>
+        public int GiveCash(int amount)
+        {
+            if(amount <= 0)
+            {
+                Console.WriteLine($"{Name} says: {amount} isn't a valid amount.");
+                return 0;
+            }
+            if(amount > Cash)
+            {
+                Console.WriteLine($"{Name} says: I dont have enough cash to give you {amount}.");
+                return 0;
+            }
+            Cash -= amount;
+            WriteMyInfo();
+            return amount;
+        }
+
+        /// <summary>
+        /// Receive some cash, adding it to my wallet (if amount is invalid
+        /// print a message to the console).
+        /// </summary>
+        /// <param name="amount">Amount of cash to tive.</param>
+        public void ReceiveCash(int amount)
+        {
+            if(amount <=0)
+            {
+                Console.WriteLine($"{Name} says: {amount} isnt an amount I'll take");
+            }
+            else
+            {
+                Cash += amount;
+                WriteMyInfo();
+            }
+        }
+    }
+}
---
+++ MoneyTracking/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MoneyTracking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guy joe = new Guy() { Name = "Joe", Cash = 50 };
+            Guy bob = new Guy() { Name = "Bob", Cash = 100 };
+
+            joe.WriteMyInfo();
+            bob.WriteMyInfo();
+
+            while(true)
+            {
+                Console.Write("Enter an amount: ");
+                string howMuch = Console.ReadLine();
+                if(howMuch == "")
+                {
+                    return;
+                }
+                if(int.TryParse(howMuch, out int result))
+                {
+                    Console.Write("Who should give the cash? ");
+                    string whichGuy = Console.ReadLine();
+                    if (whichGuy == "Joe")
+                    {
+                        joe.GiveCash(result);
+                        bob.ReceiveCash(result);
+                    }
+                    else if(whichGuy == "Bob")
+                    {
+                        bob.GiveCash(result);
+                        joe.ReceiveCash(result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter 'Joe' or 'Bob'.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an amount or a blank line to exit");
+                }
+            }
+        }
+    }
+}
